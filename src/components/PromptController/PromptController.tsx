112368fdba18f3630bb103366aa8e838d5022503
Grzegorz Dubiel <greg2012201@gmail.com>
2023-06-06 01:22:34 +0200

    Add placeholder.

--- src/components/PromptController/PromptController.tsx
+++ src/components/PromptController/PromptController.tsx
@@ -4,7 +4,12 @@ import SendButton from '@/components//SendButton/SendButton';
 function PromptController() {
   return (
     <form className='relative ml-auto mr-auto flex w-1/2 flex-col pt-8 '>
-      <TextAreaField id='answer' name='answer' className='pr-6' />
+      <TextAreaField
+        id='answer'
+        name='answer'
+        placeholder='Type your answer here...'
+        className='pr-6'
+      />
       <div className='flex-start flex w-full'>
         <SendButton className='absolute bottom-2 right-3.5' />
       </div>
--- src/components/TextAreaField/TextAreaField.tsx
+++ src/components/TextAreaField/TextAreaField.tsx
@@ -4,20 +4,22 @@ type Props = {
   id: string;
   name: string;
   className?: string;
+  placeholder?: string;
 };
 
-function TextAreaField({ id, name, className }: Props) {
+function TextAreaField({ id, name, className, placeholder }: Props) {
   return (
-    <textarea
-      className={twMerge(
-        'block w-full rounded-lg border-2 p-2.5 text-sm text-neutral-500 outline-none duration-200 focus:border-green-400',
-        className,
-      )}
-      id={id}
-      name={name}
-    >
-      Type your answer here...
-    </textarea>
+    <>
+      <textarea
+        className={twMerge(
+          'block w-full rounded-lg border-2 p-2.5 text-sm text-neutral-500 outline-none duration-200 focus:border-green-400',
+          className,
+        )}
+        id={id}
+        name={name}
+        placeholder={placeholder}
+      />
+    </>
   );
 }
 
