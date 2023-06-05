66a8be07cda43c2dd86288be588bbdccfde42f42
Grzegorz Dubiel <greg2012201@gmail.com>
2023-06-06 01:01:43 +0200

    Add className prop.

--- src/components/TextAreaField/TextAreaField.tsx
+++ src/components/TextAreaField/TextAreaField.tsx
@@ -1,13 +1,18 @@
+import { twMerge } from 'tailwind-merge';
+
 type Props = {
   id: string;
   name: string;
+  className?: string;
 };
 
-function TextAreaField({ id, name }: Props) {
+function TextAreaField({ id, name, className }: Props) {
   return (
     <textarea
-      className='block w-full rounded-lg border-2 p-2.5 text-sm text-gray-800   
-      outline-none duration-200 focus:border-gray-600'
+      className={twMerge(
+        'block w-full rounded-lg border-2 p-2.5 text-sm text-gray-800 outline-none duration-200 focus:border-gray-600',
+        className,
+      )}
       id={id}
       name={name}
     >
