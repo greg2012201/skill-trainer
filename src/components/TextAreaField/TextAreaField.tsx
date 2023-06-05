cd28c3411bc0af37c7bc5a0b5338aeeb21d737b5
Grzegorz Dubiel <greg2012201@gmail.com>
2023-06-06 01:29:11 +0200

    Remove vite icon.

--- src/components/TextAreaField/TextAreaField.tsx
+++ src/components/TextAreaField/TextAreaField.tsx
@@ -12,7 +12,7 @@ function TextAreaField({ id, name, className, placeholder }: Props) {
     <>
       <textarea
         className={twMerge(
-          'block w-full rounded-lg border-2 p-2.5 text-sm text-neutral-500 outline-none duration-200 focus:border-green-400',
+          'block w-full rounded-lg border-2 p-2.5 text-sm text-neutral-500 outline-none duration-200 focus:border-neutral-400',
           className,
         )}
         id={id}
