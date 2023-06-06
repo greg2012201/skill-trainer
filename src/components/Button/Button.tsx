10c371ee3d5273711c37d7b1955c8441d60362d8
Grzegorz Dubiel <greg2012201@gmail.com>
2023-06-07 01:41:42 +0200

    Add color variants.

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,9 +6,22 @@ type Props = {
   children: JSX.Element | string;
   className?: string;
   icon?: JSX.Element;
+  colorVariant?: 'green' | 'orange' | 'red';
 };
 
-function Button({ handleOnClick, children, className, icon }: Props): JSX.Element {
+const COLOR_VARIANTS = {
+  green: 'bg-green-400 hover:bg-green-300',
+  orange: 'bg-orange-400 hover:bg-orange-300',
+  red: 'bg-red-500 hover:bg-red-400',
+};
+
+function Button({
+  handleOnClick,
+  children,
+  className,
+  icon,
+  colorVariant = 'green',
+}: Props): JSX.Element {
   return (
     <button
       type='button'
@@ -16,6 +29,7 @@ function Button({ handleOnClick, children, className, icon }: Props): JSX.Elemen
       className={twMerge(
         'flex items-center rounded-md bg-green-400 p-1 text-sm text-white transition-all duration-300 hover:bg-green-300',
         className,
+        COLOR_VARIANTS[colorVariant],
       )}
     >
       {icon && icon}
