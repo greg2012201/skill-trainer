ebe435cc3c260c5fa79443afded8291c2e8cef77
Grzegorz Dubiel <greg2012201@gmail.com>
2023-06-07 01:33:44 +0200

    Add TaskGenerator to the PromptController.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import AnswerForm from './components/AnswerForm/AnswerForm';
-import PromptController from './components/PromptController';
+import PromptController from '@/components/PromptController';
+import TaskGenerator from '@/components/TaskGenerator';
 
 function App() {
   return (
     <div className='item-center flex flex-col content-center pt-6'>
       <h1 className='text-center text-4xl'>English trainer</h1>
-      <PromptController answerForm={<AnswerForm />} />
+      <PromptController taskGenerator={<TaskGenerator />} answerForm={<AnswerForm />} />
     </div>
   );
 }
