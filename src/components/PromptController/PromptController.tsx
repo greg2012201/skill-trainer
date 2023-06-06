f001c7f4eb6becbe8e0062a0627a6bc932973cb3
Grzegorz Dubiel <greg2012201@gmail.com>
2023-06-07 01:33:02 +0200

    Allow to add taskGenerator to the structure & add styles for wrapper.

--- src/components/PromptController/PromptController.tsx
+++ src/components/PromptController/PromptController.tsx
@@ -1,7 +1,12 @@
-type Props = { answerForm: JSX.Element };
+type Props = Record<'answerForm' | 'taskGenerator', JSX.Element>;
 
-function PromptController({ answerForm }: Props): JSX.Element {
-  return answerForm;
+function PromptController({ answerForm, taskGenerator }: Props): JSX.Element {
+  return (
+    <div className='relative ml-auto mr-auto flex w-1/2 flex-col pt-8'>
+      {taskGenerator}
+      {answerForm}
+    </div>
+  );
 }
 
 export default PromptController;
